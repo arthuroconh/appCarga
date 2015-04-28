da4785c0cd5ef91aba7e2be68318927dc9700805
arthuroconh <arthuro_b@outlook.com>
2015-04-27 22:51:39 -0430

    comit nuevo
    arreglo

--- www/buscar.php
+++ www/buscar.php
@@ -4,9 +4,9 @@
 	mysql_select_db("desarrol_seg",$conexion) or
 	die("Problemas en la seleccion de la base de datos");
 $choice =$_POST["button"]; 
-	$f=mysql_query("SELECT * FROM `t_pro` WHERE `pro_id`=$choice");
+	$f=mysql_query("SELECT * FROM `OP` ORDER BY `id` DESC limit 1");
 	$j=mysql_fetch_array($f);
-$cars = $j['pro_pro']; 
-$bikes = $j['pro_pro']; 
+$cars = $j['img']; 
+$bikes = $j['img']; 
 if($choice == "1") print json_encode($cars); 
 else print json_encode($bikes); ?>
